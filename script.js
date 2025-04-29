fc530f50715f86b683f4106f5b5324d0666bca97
joaopantoja <joaopantojacomercial@gmail.com>
2025-04-28 23:33:21 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
-// Placeholder para interações futuras
-console.log('Script carregado!');
+new TradingView.widget({
+    "width": "100%",
+    "height": "100%",
+    "symbol": "BINANCE:ETHUSDT",
+    "interval": "1",
+    "timezone": "Etc/UTC",
+    "theme": "dark",
+    "style": "1",
+    "locale": "br",
+    "toolbar_bg": "#000000",
+    "enable_publishing": false,
+    "allow_symbol_change": true,
+    "container_id": "tradingview_chart"
+});
