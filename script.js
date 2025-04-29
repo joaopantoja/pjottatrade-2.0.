208b027900c0b3548d5f5b5c5c0984407a13aad0
joaopantoja <joaopantojacomercial@gmail.com>
2025-04-28 23:58:53 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,14 +1,31 @@
+// Simulação de leitura de padrões (fictício para fins visuais)
+let sinal = document.getElementById("sinalAtual");
+
+function avaliarMercado() {
+    const chance = Math.random() * 100;
+
+    if (chance > 90) {
+        sinal.innerText = Math.random() > 0.5 ? "Compra Forte na próxima vela!" : "Venda Forte na próxima vela!";
+    } else {
+        sinal.innerText = "Aguardando sinal...";
+    }
+}
+
+setInterval(avaliarMercado, 10000);
+
+// TradingView embed
 new TradingView.widget({
-    "width": "100%",
-    "height": "100%",
-    "symbol": "BINANCE:ETHUSDT",
-    "interval": "1",
-    "timezone": "Etc/UTC",
-    "theme": "dark",
-    "style": "1",
-    "locale": "br",
-    "toolbar_bg": "#000000",
-    "enable_publishing": false,
-    "allow_symbol_change": true,
-    "container_id": "tradingview_chart"
+  "width": "100%",
+  "height": 400,
+  "symbol": "FX:EURUSD",
+  "interval": "1",
+  "timezone": "Etc/UTC",
+  "theme": "dark",
+  "style": "1",
+  "locale": "br",
+  "toolbar_bg": "#f1f3f6",
+  "enable_publishing": false,
+  "hide_top_toolbar": false,
+  "save_image": false,
+  "container_id": "tradingview_chart"
 });
